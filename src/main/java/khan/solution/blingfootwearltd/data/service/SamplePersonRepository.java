9fd08acdca58111028d7babd807592b0fa98bf72
start.vaadin.com <no-reply@vaadin.com>
2021-10-23 06:39:36 +0000

    Generated project

---
+++ src/main/java/khan/solution/blingfootwearltd/data/service/SamplePersonRepository.java
@@ -0,0 +1,10 @@
+package khan.solution.blingfootwearltd.data.service;
+
+import khan.solution.blingfootwearltd.data.entity.SamplePerson;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, Integer> {
+
+}
---
+++ src/main/java/khan/solution/blingfootwearltd/data/service/SamplePersonService.java
@@ -0,0 +1,24 @@
+package khan.solution.blingfootwearltd.data.service;
+
+import khan.solution.blingfootwearltd.data.entity.SamplePerson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.vaadin.artur.helpers.CrudService;
+import java.time.LocalDate;
+
+@Service
+public class SamplePersonService extends CrudService<SamplePerson, Integer> {
+
+    private SamplePersonRepository repository;
+
+    public SamplePersonService(@Autowired SamplePersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected SamplePersonRepository getRepository() {
+        return repository;
+    }
+
+}
